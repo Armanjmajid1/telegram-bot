dd0bc3d038b9a9e91f283e3fec9ff7847152a23c
Armanjmajid1 <151807625+Armanjmajid1@users.noreply.github.com>
2025-10-27 03:39:28 +0100

    Add files via upload

--- musicbot.py
+++ musicbot.py
@@ -13,12 +13,12 @@ def start(message):
     markup = types.InlineKeyboardMarkup()
     markup.add(
         types.InlineKeyboardButton("🎥 ڤیدیۆ", callback_data="video"),
-        types.InlineKeyboardButton("🎵 ستران", callback_data="audio")
+        types.InlineKeyboardButton("🎵 گۆرانی", callback_data="audio")
     )
     bot.send_message(
         message.chat.id,
-        "👋 سلاف! بوت بو دالنوت كرنا ستران و فيديويا ل هه مي جهان📽️.\n"
-        "لينكئ  YouTube یان TikTok يان instgarm يان هه ر جهه كي هه بيت فريكه🔗 👇",
+        "👋 سڵاو! من بۆتی گۆرانی و ڤیدیۆم.\n"
+        "لینکێکی YouTube یان TikTok بنێرە 👇",
         reply_markup=markup
     )
 
@@ -26,12 +26,12 @@ def start(message):
 def ask_url(call):
     bot.answer_callback_query(call.id)
     mode = call.data
-    msg = bot.send_message(call.message.chat.id, "🔗 لينكي فريكه :")
+    msg = bot.send_message(call.message.chat.id, "🔗 لینکەکە بنێرە:")
     bot.register_next_step_handler(msg, lambda m: download_media(m, mode))
 
 def download_media(message, mode):
     url = message.text.strip()
-    msg = bot.send_message(message.chat.id, "⏳ جه ند چركا خو بگره  نوكه دالنوت بيت🖲️...")
+    msg = bot.send_message(message.chat.id, "⏳ دابەزاندن دەستپێدەکات...")
     try:
         ydl_opts = {'outtmpl': 'downloads/%(title)s.%(ext)s', 'quiet': True}
         if mode == "audio":
@@ -53,9 +53,9 @@ def download_media(message, mode):
                 bot.send_video(message.chat.id, f, caption=f"🎬 {title}")
         os.remove(file_path)
         bot.delete_message(message.chat.id, msg.id)
-        bot.send_message(message.chat.id, "✅ نوكه دالنوت بو ب خوش حالي🌺!")
+        bot.send_message(message.chat.id, "✅ دابەزاندن تەواو بوو!")
     except Exception as e:
-        bot.send_message(message.chat.id, f"⚠️ ئاريشه ك جيبو:\n{e}")
+        bot.send_message(message.chat.id, f"⚠️ هەڵەیەک ڕویدا:\n{e}")
 
 print("🎵 Music Bot is running...")
 bot.infinity_polling()
